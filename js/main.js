194c6b76ef11e660e72c83ff96463d282047aedd
Damian Jackiewicz <damian.jackiewicz92@gmail.com>
2019-11-27 19:08:13 +0100

    Add transitionend

--- js/main.js
+++ js/main.js
@@ -2,10 +2,18 @@ window.addEventListener('keydown', (e) => {
     const divs = document.querySelectorAll(`div[data-key="${e.keyCode}"]`);
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
+    // Validate if we keydown other buttons on keyboard
     if (divs.length < 1) {
         return
     } else {
         audio.play()
     }
-    divs.forEach(div => div.classList.add("playing"))
+
+    // Add class for each div with active keydown
+    divs.forEach(div => div.classList.add("playing"));
+
+    // Remove class "playing" after end of transition
+    divs.forEach(div => div.addEventListener('transitionend', () => {
+        div.classList.remove("playing");
+    }))
 })
